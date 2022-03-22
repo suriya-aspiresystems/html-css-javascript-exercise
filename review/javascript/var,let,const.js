450705e2acd655c5ac51e49b33f466b843cc626a
suriya-aspiresystems <97100181+suriya-aspiresystems@users.noreply.github.com>
2022-03-22 17:05:42 +0530

    Add files via upload

---
+++ review/javascript/var,let,const.js
@@ -0,0 +1,23 @@
+
+        var a=10;
+        function f(){
+            var t=0;
+            console.log(a);
+        }
+        f();
+        console.log("<br>");
+        console.log(a);
+        console.log("<br>");
+        
+        console.log("<br>");
+        let b=10;
+        function fun(){
+            let c=5;
+            console.log(c);
+            console.log("<br>");
+            console.log(b);
+        }
+        console.log("<br>");
+        console.log(b);
+        console.log("<br>");
+        fun();
