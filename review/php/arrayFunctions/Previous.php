91735bfc4c7eec75bde4d4fc38c324b810fb36c7
suriya-aspiresystems <97100181+suriya-aspiresystems@users.noreply.github.com>
2022-04-12 06:01:08 +0530

    Create Previous.php

---
+++ review/php/arrayFunctions/Previous.php
@@ -0,0 +1,10 @@
+<?php
+   $input = array('foot', 'bike', 'car', 'plane');
+   
+   $mode = current($input);
+   print "$mode <br />";
+   
+   $mode = prev($input);
+   print "$mode <br />";
+   
+?>
