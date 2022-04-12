bf336aaa9d3c86c8b97064b06cafcc50ae5e91f9
suriya-aspiresystems <97100181+suriya-aspiresystems@users.noreply.github.com>
2022-04-12 06:07:46 +0530

    Create End.php

---
+++ review/php/arrayFunctions/End.php
@@ -0,0 +1,7 @@
+<?php
+   $transport = array('foot', 'bike', 'car', 'plane');
+   $mode = current($transport); 
+   print "$mode <br />";
+   $mode = end($transport);   
+   print "$mode <br />";
+   ?>
