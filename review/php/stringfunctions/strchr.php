804e573c4ab5b41952021ef4a451049d785f6628
suriya-aspiresystems <97100181+suriya-aspiresystems@users.noreply.github.com>
2022-04-12 05:36:29 +0530

    Create strchr.php

---
+++ review/php/stringfunctions/strchr.php
@@ -0,0 +1,9 @@
+<?php  
+  $str1="Hello PHP";  
+  $str2="PHP";  
+  echo "Your first string is:".$str1;  
+  echo "<br>";  
+  echo "Your second string is:".$str2;  
+  echo "<br>";  
+  echo "By using 'strrchr()' function:".strrchr($str1,$str2);  
+?>
