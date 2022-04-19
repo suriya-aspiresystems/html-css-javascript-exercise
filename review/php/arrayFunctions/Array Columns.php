591b2583f3e356e01d53c742623705ebe28fb390
suriya-aspiresystems <97100181+suriya-aspiresystems@users.noreply.github.com>
2022-04-19 17:43:03 +0530

    Create Array Columns.php

---
+++ review/php/arrayFunctions/Array Columns.php
@@ -0,0 +1,26 @@
+$records = array(
+    array(
+        'id' => 3145,
+        'first_name' => 'John',
+        'last_name' => 'Doe',
+    ),
+    array(
+        'id' => 3245,
+        'first_name' => 'Sally',
+        'last_name' => 'Smith',
+    ),
+    array(
+        'id' => 5342,
+        'first_name' => 'Jane',
+        'last_name' => 'Jones',
+    ),
+    array(
+        'id' => 5623,
+        'first_name' => 'Peter',
+        'last_name' => 'Doe',
+    )
+);
+ 
+$first_names = array_column($records, 'id');
+print_r($first_names);
+?>
