191b8c30152bf38938000d95b269c53cb44bfc08
suriya-aspiresystems <97100181+suriya-aspiresystems@users.noreply.github.com>
2022-04-21 22:26:16 +0530

    Create prev.php

---
+++ review/php/arrayFunctions/prev.php
@@ -0,0 +1,8 @@
+<?php
+$transport = array('foot', 'bike', 'car', 'plane');
+$mode = current($transport);
+$mode = next($transport);    
+$mode = next($transport);    
+$mode = prev($transport);    
+$mode = end($transport);    
+?>
