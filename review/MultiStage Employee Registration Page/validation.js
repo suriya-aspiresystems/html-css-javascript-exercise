fc742c9ee895f4ffdc0d26f65f7f2e04cea9bc6d
suriya-aspiresystems <97100181+suriya-aspiresystems@users.noreply.github.com>
2022-04-27 17:50:59 +0530

    Update validation.js

--- review/MultiStage Employee Registration Page/validation.js
+++ review/MultiStage Employee Registration Page/validation.js
@@ -1,149 +1,121 @@
-function next_step1() {
-    document.getElementById("first").style.display = "none";
-    document.getElementById("second").style.display = "block";
+function nextTab(event) {
+    var parent = event.target.parentElement;
+    parent.classList.remove('active');
+    parent.nextElementSibling.classList.add('active');
+}
     
-    }
+function previousTab(event) {
+    var parent = event.target.parentElement;
+    parent.classList.remove('active');
+    parent.previousElementSibling.classList.add('active');
+}
     
-    function fprev() {
-        document.getElementById('first').style.display='block';
-        document.getElementById('second').style.display='none';
-        document.getElementById('third').style.display='none';
-         document.getElementById('fourth').style.display='none';
-        document.getElementById('fifth').style.display='none';
-    }
-    function fnext() {
-        document.getElementById('second').style.display='none';
-        document.getElementById('third').style.display='block';
-    }
-    function thirdNext() {
-        document.getElementById('first').style.display='none';
-        document.getElementById('second').style.display='none';
-        document.getElementById('third').style.display='none';
-        document.getElementById('fourth').style.display='block';
-        document.getElementById('fifth').style.display='none';
-    }
-    function tprev() {
-        document.getElementById('first').style.display='none';
-        document.getElementById('second').style.display='block';
-        document.getElementById('third').style.display='none';
-        document.getElementById('fourth').style.display='none';
-        document.getElementById('fifth').style.display='none';
-    }
-    function fNext(){
-        document.getElementById('first').style.display='none';
-        document.getElementById('second').style.display='none';
-        document.getElementById('third').style.display='none';
-        document.getElementById('fourth').style.display='none';
-        document.getElementById('fifth').style.display='block';
-    }
-    function fPrev(){
-        document.getElementById('first').style.display='none';
-        document.getElementById('second').style.display='none';
-        document.getElementById('third').style.display='block';
-        document.getElementById('fourth').style.display='none';
-        document.getElementById('fifth').style.display='none';
-    }
-    function Prev(){
-        document.getElementById('first').style.display='none';
-        document.getElementById('second').style.display='none';
-        document.getElementById('third').style.display='none';
-        document.getElementById('fourth').style.display='block';
-        document.getElementById('fifth').style.display='none';
-    }
     var form = document.getElementById('next_btn1');
-    var username = document.getElementById('username');
     var email = document.getElementById('email');
-    var password = document.getElementById('password');
-    var password2 = document.getElementById('pass');
-    const number=document.getElementById('number');
-   
-    email.onchange = (event) => {
+    email.oninput = (event) => {
         validations(event.target,/^[A-Za-z.0-9]{2,}@[a-z]{2,}\.[a-z]{2,}$/,'Invalid email');
     };
-    
-    password.onchange = (event) => {
-        validations(event.target,/[A-Z]{1,}[a-z]{6,}[0-9]{1,}/,'Invalid password');
-    };
-    
-    password2.onchange = (event) => {
-        if(password.value == event.target.value)
-            event.target.nextElementSibling.textContent = '';
-        else
-        {
+    var password = document.getElementById('password');
+    password.oninput = (event) => {
+        validations(event.target,  /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{6,16}$/,'Invalid password');
+    };  
+    var confirmPassword = document.getElementById('ConfirmPassword');
+    confirmPassword.oninput = (event) => {
+        var parent=event.target.parentElement;
+        if(password.value == event.target.value){
+             event.target.nextElementSibling.textContent = '';
+             parent.classList.add('success');
+        }
+        else{
             displayMessage(event.target,'Both passwords must be same');
+            if(parent.classList.contains('success')){
+                parent.classList.remove('success');
+            }
+            parent.classList.add('error');
         }
     }
-    
-    username.onchange = (event) => {
+    var username = document.getElementById('username');
+    username.oninput = (event) => {
         validations(event.target,/^[A-za-z]{5,}$/,'Invalid name');
     }
     function validations(element,pattern,message)
     {
-        if(pattern.test(element.value))
+        var parent= element.parentElement;
+        if(pattern.test(element.value)){
+            
             element.nextElementSibling.textContent = '';
-        else
-        {
-            displayMessage(element,message);
+            parent.classList.add('success');
+            
         }
+        else{
+            displayMessage(element,message);
+            if(parent.classList.contains('success')){
+               parent.classList.remove('success');
+             }
+             parent.classList.add('error');
+        }
+
     }
     
     function displayMessage(element,message)
     {
         element.nextElementSibling.textContent = message;
-        element.value='';
     }
     
-    next_btn1.onclick = () => {
+     next_btn1.onclick = (event) => {
         var inputFields = document.getElementsByClassName('text_field');
-        var flag = true;
+         var flag = true;
+         var flag1 = true;
         for(index=0;index<inputFields.length;index++)
         {
+            var alertBox = document.getElementsByClassName('alertbox');
+            for(j=0;j<alertBox.length;j++)
+            {
+                if(alertBox[j].textContent)
+                {
+                    flag1 = false;
+                    break;
+                }
+            }
             if(inputFields[index].value=='')
             {
                 flag=false;
                 inputFields[index].nextElementSibling.textContent = 'Please fill this field';
             }
+            flag = flag && flag1;
         }
-        if(flag)
-        next_step1();
+        if(flag){
+           nextTab(event);
+        }
     }
-    date.onchange=(event)=>{
+    date.oninput=(event)=>{
         var date=event.target.value;//date in String
         var dateFormat=new Date(date);//date in date
         var currentDate=new Date();
         var dateDiff=currentDate.getFullYear()-dateFormat.getFullYear();
+        var parent = event.target.parentElement;
         if(dateDiff>=18 && dateDiff<=60){
             event.target.nextElementSibling.textContent = '';
+            parent.classList.add('success');
         }
         else{
             displayMessage(event.target,"Age Should be between 18 to 60");
+            if(parent.classList.contains('success')){
+                parent.classList.remove('success');
+            }
+            parent.classList.add('error');
         }
-
     }
-    number.onchange = (event) => {
+    var number=document.getElementById('number');
+    number.oninput = (event) => {
         validations(event.target,/^[6789][0-9]{9}$/,'Invalid Mobile Number');
     };
   
-    
-    function validations(element,pattern,message)
-    {
-        if(pattern.test(element.value))
-            element.nextElementSibling.textContent = '';
-        else
-        {
-            displayMessage(element,message);
-        }
-    }
-    
-    function displayMessage(element,message)
-    {
-        element.nextElementSibling.textContent = message;
-        element.value='';
-    }
-    next.onclick = () => {
+    next.onclick = (event) => {
         var inputFields = document.getElementsByClassName('input2');
         var flag = true;
         var isChecked = false;
+        var flag1 = true;
         for(index=0;index<inputFields.length;index++)
         {
             var radio = document.getElementsByName('gender');
@@ -156,22 +128,36 @@ function next_step1() {
                     break;
                 }
             }
-            if(!isChecked)
+            var alertBox = document.getElementsByClassName('alertbox1');
+            for(j=0;j<alertBox.length;j++)
+            {
+                if(alertBox[j].textContent && alertBox[j].textContent == 'Please fill in this field')
+                {
+                    flag1 = false;
+                    break;
+                }
+            }
+            if(!isChecked){
                 parent.lastElementChild.textContent = 'Please fill in this field';
+
+            }
+            else{
+                parent.lastElementChild.textContent='';
+            }
 
             if(inputFields[index].value=='' || inputFields[index].selectedIndex == '0')
             {
                 flag=false;
                 inputFields[index].nextElementSibling.textContent = 'Please fill this field';
             }
-            flag = isChecked;
+            flag = flag && isChecked && flag1;
         }
         if(flag)
         {
-           fnext();
+           nextTab(event);
         }
     }
-    next_btn3.onclick = () =>{
+    next_btn3.onclick = (event) =>{
         var inputFields=document.getElementsByClassName('input_text');
         var flag=true;
         for(i=0;i<inputFields.length;i++){
@@ -186,26 +172,33 @@ function next_step1() {
             }
         }
         if(flag){
-            fNext();
+            nextTab(event);         
         }
         
     }
-    check.onclick = (event) => {
-        var practice = document.getElementById('practice');
-        if(practice.selectedIndex == '0')
-        {
-            event.target.checked = false;
-            practice.nextElementSibling.textContent = 'Please fill in this field';
-        }
-        else
-            practice.nextElementSibling.textContent = '';
-        if(event.target.checked)
-        {
-            document.getElementById('btn').disabled = false;
-        }
-        else
-        {
-            document.getElementById('btn').disabled = true;
-        }
+btn.onclick = (event) => {
+    var flag = true;
+    if(document.getElementById('practice').selectedIndex!='0')
+    {
+        document.getElementById('practice').nextElementSibling.textContent = '';
     }
-  
+    else
+    {
+        document.getElementById('practice').nextElementSibling.textContent = 'Please fill in this field';
+        flag = false;
+    }
+    if(document.getElementById('check').checked)
+    {
+        document.getElementById('check').nextElementSibling.textContent = '';
+    }
+    else
+    {
+        document.getElementById('check').nextElementSibling.textContent = 'Please fill in this field';
+        flag = false;
+    }
+    if(flag)
+    {
+        console.log(document.getElementById('form'));
+        document.getElementById('form').submit();
+    }
+}
