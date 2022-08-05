2b030a8314931bc4e01dc969e36f56e4736e170c
suriya-aspiresystems <97100181+suriya-aspiresystems@users.noreply.github.com>
2022-08-05 17:38:01 +0530

    Update config.php

--- Employee_Management_System/config/config.php
+++ Employee_Management_System/config/config.php
@@ -1,1 +1,15 @@
 
+<?php
+
+   //Database parameters
+   define('DB_HOST', 'localhost');
+   define('DB_USER', 'root');
+   define('DB_PASSWORD', 'Aspire@123');
+   define('DB_NAME', 'employee_management_system');
+   //APP ROOT
+   define('APPROOT', dirname(dirname(__FILE__)));
+   
+   //URL ROOT
+   define('URLROOT', 'employeemanagementsystem.com');
+
+   define('SITENAME', 'EMPLOYEE_MANAGEMENT_SYSTEM');
